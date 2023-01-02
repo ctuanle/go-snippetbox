e05eb15c2d9ba9bf8ac05857f69f85463f68e2d1
Le Cong Tuan <congtuanle23@gmail.com>
2023-01-03 00:59:46 +0100

    c12: using request context

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -68,5 +68,10 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 
 // isAuthenticated() returns true if a request is authenticated
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isAuthenticated
 }
